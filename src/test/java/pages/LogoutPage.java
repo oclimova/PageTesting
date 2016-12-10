049b4917f5002329e3c7344618fc0c9d69c85fc3
Olga Climova <olga.climova@endava.com>
2016-12-10 13:35:28 +0200

    LogoutPage model created

---
+++ src/test/java/pages/LogoutPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by oclimova on 12/10/2016.
+ */
+public class LogoutPage extends AbstractPage {
+    public LogoutPage(WebDriver webdriver) {
+        super(webdriver);
+    }
+
+    public LogoutPage navigate() {
+        getDriver().get("http://172.16.44.49/oc/index.php?route=account/logout");
+        return this;
+    }
+
+    public boolean weAreHere() {
+        return getDriver().getTitle().equals("Account Logout");
+    }
+}
