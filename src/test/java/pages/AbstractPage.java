2aedfd9ce8087e3212024e357f1815bdc9793b0e
Olga Climova <olga.climova@endava.com>
2016-12-12 17:54:32 +0200

    login, createAccount, logout By objects and methods moved to StoreAbstractPage class

--- src/test/java/pages/AbstractPage.java
+++ src/test/java/pages/AbstractPage.java
@@ -4,15 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public abstract class AbstractPage {
+public abstract class AbstractPage implements Page {
     private WebDriver driver;
-
-    private By login = By.xpath(".//*[text()='login']/.."),
-            createAccount = By.xpath(".//*[text()='create an account']/.."),
-            logout = By.xpath(".//*[text()='Logout']/..");
-
-    public abstract AbstractPage navigate();
-    public abstract boolean isActive();
 
     public AbstractPage(WebDriver driver) {
         this.driver = driver;
@@ -20,23 +13,6 @@ public abstract class AbstractPage {
 
     public WebDriver getDriver() {
         return driver;
-    }
-
-    public AbstractPage logout() {
-        WebElement logoutLink = driver.findElement(logout);
-        if (logoutLink != null)
-            logoutLink.click();
-        return this;
-    }
-
-    public AbstractPage clickLogin() {
-        getDriver().findElement(login).click();
-        return this;
-    }
-
-    public AbstractPage clickCreateAccount() {
-        getDriver().findElement(createAccount).click();
-        return this;
     }
 
     protected String getElementText(By element) {
--- src/test/java/pages/StoreAbstractPage.java
+++ src/test/java/pages/StoreAbstractPage.java
@@ -1,4 +1,27 @@
 package pages;
 
-public abstract class StoreAbstractPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public abstract class StoreAbstractPage extends AbstractPage {
+    private By login = By.xpath(".//*[text()='login']/.."),
+            createAccount = By.xpath(".//*[text()='create an account']/.."),
+            logout = By.xpath(".//*[text()='Logout']/..");
+
+    public AbstractPage logout() {
+        WebElement logoutLink = getDriver().findElement(logout);
+        if (logoutLink != null)
+            logoutLink.click();
+        return this;
+    }
+
+    public AbstractPage clickLogin() {
+        getDriver().findElement(login).click();
+        return this;
+    }
+
+    public AbstractPage clickCreateAccount() {
+        getDriver().findElement(createAccount).click();
+        return this;
+    }
 }
