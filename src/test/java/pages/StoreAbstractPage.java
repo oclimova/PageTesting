75a95ce944ec784ec71179323065c577f25392c9
Olga Climova <olga.climova@endava.com>
2016-12-12 17:55:27 +0200

    Constructor added to StoreAbstractPage

--- src/test/java/pages/StoreAbstractPage.java
+++ src/test/java/pages/StoreAbstractPage.java
@@ -1,12 +1,17 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public abstract class StoreAbstractPage extends AbstractPage {
     private By login = By.xpath(".//*[text()='login']/.."),
             createAccount = By.xpath(".//*[text()='create an account']/.."),
             logout = By.xpath(".//*[text()='Logout']/..");
+
+    public StoreAbstractPage(WebDriver driver) {
+        super(driver);
+    }
 
     public AbstractPage logout() {
         WebElement logoutLink = getDriver().findElement(logout);
