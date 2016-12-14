8f14dc4ca1de22d5c8069db394b8b42ff600732a
Olga Climova <olga.climova@endava.com>
2016-12-14 09:20:05 +0200

    AccountCreated page model created

--- src/test/java/pages/AccountCreatedPage.java
+++ src/test/java/pages/AccountCreatedPage.java
@@ -1,7 +1,31 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by oclimova on 12/14/2016.
  */
-public class AccountCreatedPage {
+public class AccountCreatedPage extends StoreAbstractPage {
+    private static final String PATH = "index.php?route=account/success";
+
+    public AccountCreatedPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getPath() {
+        return super.getPath() + PATH;
+    }
+
+    public AccountCreatedPage navigate() {
+        getDriver().get(getPath());
+        return this;
+    }
+
+    public boolean isCurrentPage() {
+        return isCurrentPage(getDriver());
+    }
+
+    public static boolean isCurrentPage(WebDriver driver) {
+        return driver.getTitle().equals("Your Account Has Been Created!");
+    }
 }
